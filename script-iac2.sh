0e4f1eda45b2bdeac24357339c95fb34fb60c675
wjunes <willans@tuyaymia.com>
2022-08-18 21:18:01 +0000

    Apache v.1.0

---
+++ script-iac2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Atualizando servidor..."
+
+apt-get update
+apt-get upgrade
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando e copiando arquivos da aplicacao... "
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+
